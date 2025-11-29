5b4c922aeaaa99323039618a6003d9b6edf3eedc
Marc Roussel <marc.roussel@universite-paris-saclay.fr>
2025-11-29 23:49:51 +0100

    Fin du 2-2

--- 02-controle/exercice2.cpp
+++ 02-controle/exercice2.cpp
@@ -37,28 +37,35 @@
 #include <array>
 #include <iostream>
 
-void afficher_grille(std::array< std::array < char, 3 >, 3> grille) {
-  std::cout << "___0___1___2_\n";
-  std::cout << "_____________\n";
+void afficher_grille(std::array<std::array<char, 3>, 3> grille)
+{
+    std::cout << "___0___1___2_\n";
 
-  // Parcours des lignes de la grille
-  for (int i = 0; i < (int)grille.size(); i++) {
-    std::cout << i << "|";
+    // Parcours des lignes de la grille
+    for (int i = 0; i < (int)grille.size(); i++)
+    {
+        std::cout << i << "|";
 
-    // Parcours des cases de la ligne
-    for (int j = 0; j < (int)grille[i].size(); j++) {
-      std::cout << " " << grille[i][j] << " |";
+        // Parcours des cases de la ligne
+        for (int j = 0; j < (int)grille[i].size(); j++)
+        {
+            std::cout << " " << grille[i][j] << " |";
+        }
+
+        std::cout << "\n_____________\n";
     }
-
-    std::cout << "_____________\n";
-  }
 }
 
-int main(int, char **) { 
+int main(int, char **)
+{
 
     // Q1
     // Définition de la grille
-    std::array< std::array < char, 3 >, 3> grille;
+    std::array<std::array<char, 3>, 3> grille = {{
+        {' ', ' ', ' '},
+        {' ', ' ', ' '},
+        {' ', ' ', ' '}
+    }};
 
     // Q2
     // Variable définissant la fin
@@ -69,32 +76,177 @@ int main(int, char **) {
     char symbole = 'X';
 
     // Variables définissant les coordonnées du prochain coup
-    int x;
-    int y;
+    int col;
+    int row;
 
-    while (!fin) {
+    while (!fin)
+    {
 
-      // Q4
-      // Affichage de la grille
-      afficher_grille(grille);
-      
-      // Entrée utilisateur
-      std::cout << "\n" << symbole << ", à vous de jouer ! Sélectionnez les coordonnées de la case à jouer (colonne puis ligne) \n";
+        // Q4
+        // Affichage de la grille
+        afficher_grille(grille);
 
-      std::cout << "Colonne : ";
-      std::cin >> x;
+        // Entrée utilisateur
+        std::cout << "\n"
+                  << symbole << ", à vous de jouer ! Sélectionnez les coordonnées de la case à jouer (colonne puis ligne) \n";
 
-      std::cout << "Ligne : ";
-      std::cin >> y;
+        std::cout << "Colonne : ";
+        std::cin >> col;
 
-      // Q3
-      if (symbole == 'X') {
-        symbole = 'O';
-      }
-      else {
-        symbole = 'X';
-      }
+        std::cout << "Ligne : ";
+        std::cin >> row;
 
+        // Boucle de redemande si présence d'erreur
+        bool redemander = true;
+        while (redemander)
+        {
+
+            // Vérification de la bonne saisie des coordonnées
+            if (col <= 2 && col >= 0 && row <= 2 && row >= 0)
+            {
+                // Vérification que la case est bien vide
+                if (grille[row][col] == ' ')
+                {
+                    redemander = false;
+                    continue;
+                }
+            }
+
+            if (redemander)
+            {
+                // Redemande à l'utilisateur
+                std::cout << "Désolé, votre saisie est incorrecte. Sélectionnez les coordonnées de la case à jouer (colonne puis ligne) \n";
+
+                std::cout << "Colonne : ";
+                std::cin >> col;
+
+                std::cout << "Ligne : ";
+                std::cin >> row;
+            }
+        }
+        
+        // Changement de la valeur de la case
+        grille[row][col] = symbole;
+
+        // Déclaration du gagnant
+        char gagnant = ' ';
+
+        /// Vérifications de gain 
+        // Par ligne
+        for (int i = 0; i < (int)grille.size(); i++)
+        {
+            bool est_gagnant = true;
+            char caractere_a_verif = grille[i][0];
+
+            std::cout << "(i) DEBUG | caractere_a_verif = " << caractere_a_verif << "\n";
+
+            for (int j = 1; j < (int)grille[i].size(); j++)
+            {
+                if (grille[i][j] != caractere_a_verif || caractere_a_verif == ' ') {
+                    est_gagnant = false;
+                }
+            }
+
+            std::cout << "(i) DEBUG | est_gagnant = " << est_gagnant << "\n";
+
+            if (est_gagnant) {
+                fin = true;
+                gagnant = caractere_a_verif;
+            }
+        }
+
+        // Par colonne
+        for (int i = 0; i < (int)grille[i].size(); i++)
+        {
+            bool est_gagnant = true;
+            char caractere_a_verif = grille[0][i];
+
+            for (int j = 1; j < (int)grille.size(); j++)
+            {
+                if (grille[j][i] != caractere_a_verif || caractere_a_verif == ' ') {
+                    est_gagnant = false;
+                }
+            }
+
+            if (est_gagnant) {
+                fin = true;
+                gagnant = caractere_a_verif;
+            }
+        }
+
+        // Par diagonale (haut-gauche | bas-droit)
+        
+        bool est_gagnant = true;
+        char caractere_a_verif = grille[0][0];
+
+        for (int i = 1; i < (int)grille.size(); i++)
+        {
+            if (grille[i][i] != caractere_a_verif || caractere_a_verif == ' ') {
+                est_gagnant = false;
+            }
+        }
+
+        if (est_gagnant) {
+            fin = true;
+            gagnant = caractere_a_verif;
+        }
+
+        est_gagnant = true;
+        caractere_a_verif = grille[2][2];
+
+        for (int i = 1; i  >= 0; i--)
+        {
+            if (grille[i][i] != caractere_a_verif || caractere_a_verif == ' ') {
+                est_gagnant = false;
+            }
+        }
+
+        if (est_gagnant) {
+            fin = true;
+            gagnant = caractere_a_verif;
+        }
+
+        // Vérification d'égalité (seulement si la fin n'est pas déclarée)
+        if (!fin) {
+            fin = true;
+            for (int i = 0; i < (int)grille.size(); i++)
+            {
+                for (int j = 0; j < (int)grille[i].size(); j++)
+                {
+                    if (grille[i][j] == ' ') {
+                        std::cout << "(i) DEBUG | (grille[i][j] == ' ') = " << (grille[i][j] == ' ') << "\n";
+                        fin = false;
+                        break;
+                    }
+                }
+            }
+        }
+
+        // Vérification de fin
+        if (fin) {
+            afficher_grille(grille);
+            
+            if (gagnant == ' '){
+                std::cout << "Égalité \n";
+            }
+            else {
+                std::cout << "Félicitations, " << gagnant << ", vous avez gagné ! \n";
+            }
+            
+        }
+
+        else {
+            // Q3
+            // Changement de symbole à la fin du tour
+            if (symbole == 'X')
+            {
+                symbole = 'O';
+            }
+            else
+            {
+                symbole = 'X';
+            }
+        }        
     }
 
     return 0;
